b97a06a504e89dec07423dfe16a590ceb4eb2dec
Rohit Udupa <Rohith.Udupa@idamta.com>
2017-12-29 17:05:55 +0530

    okta commit

---
+++ oktatest/oktatest/Startup.cs
@@ -0,0 +1,81 @@
+﻿using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication.OpenIdConnect;
+using Microsoft.IdentityModel.Tokens;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace oktatest
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            
+            
+            services.AddMvc();
+            services.AddAuthentication(sharedOptions =>
+            {
+                sharedOptions.DefaultAuthenticateScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+                sharedOptions.DefaultSignInScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+                sharedOptions.DefaultChallengeScheme = OpenIdConnectDefaults.AuthenticationScheme;
+            })
+   .AddCookie()
+   .AddOpenIdConnect(options =>
+   {
+       options.ClientId = "0oada66j914EMiUcS0h7";
+       options.ClientSecret = "4wJ2rnNxq3DV2qN6XcK2gKWaZES05p2ebwDcbe_h";
+       options.Authority = "https://dev-711615.oktapreview.com";
+       options.CallbackPath = "/authorization-code/callback";
+       options.ResponseType = "code";
+       options.SaveTokens = true;
+       options.UseTokenLifetime = false;
+       options.GetClaimsFromUserInfoEndpoint = true;
+       options.Scope.Add("openid");
+       options.Scope.Add("profile");
+       options.TokenValidationParameters = new TokenValidationParameters
+       {
+           NameClaimType = "name"
+       };
+   });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                app.UseBrowserLink();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+            }
+
+            app.UseStaticFiles();
+
+            app.UseAuthentication();
+
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Account}/{action=Login}/{id?}");
+            });
+        }
+    }
+}
